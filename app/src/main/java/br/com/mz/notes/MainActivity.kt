83f1302e525ca33211426f62703e8b390b64dd28
MZaqueu <30877800+marcuszaqueu@users.noreply.github.com>
2021-07-01 14:52:19 -0300

    Organizando import's da MainActivity.kt

--- app/src/main/java/br/com/mz/notes/MainActivity.kt
+++ app/src/main/java/br/com/mz/notes/MainActivity.kt
@@ -1,19 +1,11 @@
 package br.com.mz.notes
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import br.com.mz.notes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
